3788e236396173bd247d6b78386eb661a5c2c212
dagi931 <dagiderbe59@gmail.com>
2026-08-19 23:13:54 +0300

    init: import database configurations and security packages

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,1 +1,4 @@
-﻿
+﻿const { mainDb } = require('../config/prisma');
+const { sendSMS } = require('../utils/sms');
+const { logAction } = require('../utils/audit');
+const bcrypt = require('bcryptjs');
