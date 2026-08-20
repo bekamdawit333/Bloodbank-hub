aa4c30ed8a55bfd2527598ab1b983297ee6d6180
Bekam Dawit <bekamdawit333@gmail.com>
2026-08-20 20:04:12 +0300

    Update audit logs

--- backend/src/utils/audit.js
+++ backend/src/utils/audit.js
@@ -1,20 +1,17 @@
-const { mainDb } = require("../config/prisma");
+const { mainDb } = require('../config/prisma');
 
-async function logAction(userId, action, details, ipAddress = "127.0.0.1") {
+async function logAction(userId, action, details, ipAddress = '127.0.0.1') {
   try {
     await mainDb.auditLog.create({
       data: {
         user_id: userId,
-
         action,
-
         details,
-
-        ip_address: ipAddress,
-      },
+        ip_address: ipAddress
+      }
     });
   } catch (err) {
-    console.error("[AUDIT LOGGER ERROR]:", err.message);
+    console.error('[AUDIT LOGGER ERROR]:', err.message);
   }
 }
 
