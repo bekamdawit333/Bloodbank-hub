3052e51f83502d27766057c25974b2b4cbd48c08
beckham333 <bekamdawit333@gmail.com>
2026-08-20 07:40:31 +0300

    feat:add lab router

--- backend/src/routes/lab.js
+++ backend/src/routes/lab.js
@@ -1,1 +1,11 @@
-﻿
+﻿const express = require('express');
+const router = express.Router();
+const labController = require('../controllers/labController');
+const { authenticateToken, requireRole } = require('../middleware/auth');
+
+router.get('/samples', authenticateToken, requireRole(['laboratory']), labController.getPendingSamples);
+router.get('/warehouses', authenticateToken, requireRole(['laboratory']), labController.getWarehouses);
+router.post('/samples/:id/test', authenticateToken, requireRole(['laboratory']), labController.submitTestResult);
+
+module.exports = router;
+
